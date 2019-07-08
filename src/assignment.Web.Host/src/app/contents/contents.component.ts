e281461942a900b6a218314cf5c037dc317049c8
SupaxInc <paulj0997@gmail.com>
2019-07-08 00:27:36 -0400

    Revert "Added routing and component functions"
    This reverts commit caec87e46059ab6c3268d118c20cb48cfad91b9c.

--- src/assignment.Web.Host/src/app/contents/contents.component.ts
+++ src/assignment.Web.Host/src/app/contents/contents.component.ts
@@ -1,25 +1,13 @@
-import { Component, OnInit, Injector } from '@angular/core';
-import { ContentServiceProxy, GetContentOutput } from '@shared/service-proxies/service-proxies';
-import { AppComponentBase } from '@shared/app-component-base';
-import { MatDialog } from '@angular/material';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contents',
   templateUrl: './contents.component.html',
   styles: []
 })
-export class ContentsComponent extends AppComponentBase implements OnInit {
+export class ContentsComponent implements OnInit {
 
-  content: GetContentOutput = new GetContentOutput();
-
-  constructor(
-    injector: Injector,
-    private _contentService: ContentServiceProxy,
-    private _dialog: MatDialog
-    ) {
-      super(injector);
-     }
+  constructor() { }
 
   ngOnInit() {
   }
