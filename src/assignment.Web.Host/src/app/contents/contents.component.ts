caec87e46059ab6c3268d118c20cb48cfad91b9c
SupaxInc <paulj0997@gmail.com>
2019-07-08 00:27:21 -0400

    Added routing and component functions

--- src/assignment.Web.Host/src/app/contents/contents.component.ts
+++ src/assignment.Web.Host/src/app/contents/contents.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
+import { ContentServiceProxy, GetContentOutput } from '@shared/service-proxies/service-proxies';
+import { AppComponentBase } from '@shared/app-component-base';
+import { MatDialog } from '@angular/material';
+
 
 @Component({
   selector: 'app-contents',
   templateUrl: './contents.component.html',
   styles: []
 })
-export class ContentsComponent implements OnInit {
+export class ContentsComponent extends AppComponentBase implements OnInit {
 
-  constructor() { }
+  content: GetContentOutput = new GetContentOutput();
+
+  constructor(
+    injector: Injector,
+    private _contentService: ContentServiceProxy,
+    private _dialog: MatDialog
+    ) {
+      super(injector);
+     }
 
   ngOnInit() {
   }
