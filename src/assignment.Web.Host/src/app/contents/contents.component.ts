5644b133b1e1b562ade6ca6d1c03885d112fbd6b
SupaxInc <paulj0997@gmail.com>
2019-07-08 10:34:36 -0400

    Revert "Revert "Initial creations of contents components""
    This reverts commit 78fddf490986c92a09afede80903fc4b841f0659.

---
+++ src/assignment.Web.Host/src/app/contents/contents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contents',
+  templateUrl: './contents.component.html',
+  styles: []
+})
+export class ContentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
